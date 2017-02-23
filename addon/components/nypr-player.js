4a9bd50d3ac9699e9866174826256c43b8422318
walsh9 <walsh9@gmail.com>
2017-02-23 12:04:34 -0500

    Add semicolon

--- addon/components/nypr-player.js
+++ addon/components/nypr-player.js
@@ -33,7 +33,7 @@ export default Ember.Component.extend(KeyboardCommandMixin, {
   didInsertElement() {
     // focus on first launch
     if (get(this, 'autofocus')) {
-      next(() => { this.$().focus() });
+      next(() => { this.$().focus(); });
     }
   },
 
